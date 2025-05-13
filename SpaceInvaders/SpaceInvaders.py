117981bafbe82dd0ce23535e79a412fd77e47197
Trinity <ryankerr201@gmail.com>
2025-05-13 16:46:16 +0100

    Invader update
    Added in a single turtle that will be the beginning of the enemy code.  Set it begin in one direction, changing once it hits the map boundary and dropping down towards the 'player'

--- SpaceInvaders/SpaceInvaders.py
+++ SpaceInvaders/SpaceInvaders.py
@@ -20,18 +20,30 @@ border_pen.pensize(3)
 for side in range(4):
     border_pen.fd(600)
     border_pen.lt(90)
+border_pen.hideturtle()
 
 
  #creating the player
-player= turtle.Turtle()
+player = turtle.Turtle()
 player.color("blue")
 player.shape("triangle")
 player.penup()
 player.speed(0)
 player.lt(90)
 player.setposition(0, -250)
+player.setheading(90)
 
 playerspeed = 15
+
+#Invaders
+invader = turtle.Turtle()
+invader.color("red")
+invader.shape("circle")
+invader.penup()
+invader.speed(0)
+invader.setposition(-200, 250)
+
+invaderspeed = 2
 
 #Moving the player
 def move_left():
@@ -52,6 +64,30 @@ def move_right():
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
+
+
+#Main game loop
+while True:
+    
+    #invader movment
+    x = invader.xcor()
+    x += invaderspeed
+    invader.setx(x)
+    
+    # Inverted movment
+    if invader.xcor() > 280:
+        invaderspeed *= -1
+        y = invader.ycor()
+        y -= 30
+        invader.sety(y)
+        
+        
+    if invader.xcor() < -280:
+        invaderspeed *= -1
+        y = invader.ycor()
+        y -= 30
+        invader.sety(y)
+        
 
 
 delay = input("Press enter to finish.")
